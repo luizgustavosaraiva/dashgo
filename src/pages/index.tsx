976ecf32e476739faf392beae22c3e407f55f3b3
Luiz Gustavo Saraiva <lg.saraiva@outlook.com>
2021-12-06 21:25:16 -0300

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import {
+  Flex,
+  Button,
+  Stack,
+} from '@chakra-ui/react';
+import type { NextPage } from 'next';
+import { Input } from '../components/Form/Input';
+
+const Home: NextPage = () => {
+  return (
+    <Flex w='100vw' h='100vh' align='center' justify='center'>
+      <Flex
+        as='form'
+        w='100%'
+        maxW={360}
+        bg='gray.800'
+        p='8'
+        borderRadius={8}
+        flexDir='column'>
+        <Stack spacing={4}>
+          <Input name='email' type='email' label='E-mail' />
+        </Stack>
+        <Button type='submit' mt='6' colorScheme='pink' size='lg'>
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Home;
